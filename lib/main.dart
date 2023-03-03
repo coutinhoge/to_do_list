6b6f84ce9b8f1c1adb124354ebf38b06bc7f1f18
gabriel-079 <contatogc871@gmail.com>
2023-03-03 16:23:03 -0300

    primeira versao

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/pages/to_do_list_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ToDoListPage();
+  }
+}
